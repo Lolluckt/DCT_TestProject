8ac4d377d6749870784c499e958e07cbeb54b03f
Lolluckt <egorbl2004@gmail.com>
2025-04-17 01:56:06 +0300

    Add IsDark property and ToggleThemeCommand.

--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -1,28 +1,34 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CryptoTrackerApp.Infrastructure;
+using System.Windows;
 
 namespace CryptoTrackerApp.ViewModels
 {
     public partial class ShellViewModel : BaseViewModel
     {
         public static NavigationService CurrentNavService { get; private set; }
+        public static void SetNavService(NavigationService nav) => CurrentNavService = nav;
 
-        public static void SetNavService(NavigationService navService)
-        {
-            CurrentNavService = navService;
-        }
+        [ObservableProperty] private bool isDark;
 
-        [RelayCommand]
-        public void NavigateHome()
-        {
-            CurrentNavService.NavigateTo("MainView");
-        }
+        [RelayCommand] public void NavigateHome() => CurrentNavService.NavigateTo("MainView");
+        [RelayCommand] public void GoBack() => CurrentNavService.GoBack();
 
         [RelayCommand]
-        public void GoBack()
+        public void ToggleTheme()
         {
-            CurrentNavService.GoBack();
+            IsDark = !IsDark;
+            var dict = new ResourceDictionary
+            {
+                Source = new Uri($"/Themes/{(IsDark ? "Dark" : "Light")}.xaml", UriKind.Relative)
+            };
+
+            var appDicts = Application.Current.Resources.MergedDictionaries;
+            if (appDicts.Count == 0)
+                appDicts.Add(dict);
+            else
+                appDicts[0] = dict;
         }
     }
 }
