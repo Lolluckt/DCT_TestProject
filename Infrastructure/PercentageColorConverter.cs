73287510d15e1c1cb861929c0774c7fa2d8510f2
Lolluckt <egorbl2004@gmail.com>
2025-04-17 01:55:22 +0300

    Simplify logic; align with theme brushes.

--- Infrastructure/PercentageColorConverter.cs
+++ Infrastructure/PercentageColorConverter.cs
@@ -9,23 +9,18 @@ namespace CryptoTrackerApp.Infrastructure
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            double dVal = 0;
-            if (value is double d)
-                dVal = d;
-            else if (value is decimal dec)
-                dVal = (double)dec;
-            if (dVal > 5)
-                return new SolidColorBrush(Color.FromRgb(0, 150, 0));
-            else if (dVal > 0)
-                return new SolidColorBrush(Color.FromRgb(60, 179, 113));
-            else if (dVal < -5)
-                return new SolidColorBrush(Color.FromRgb(178, 34, 34));
-            else if (dVal < 0)
-                return new SolidColorBrush(Color.FromRgb(220, 20, 60));
-            else
-                return new SolidColorBrush(Color.FromRgb(128, 128, 128));
+            double val = value switch
+            {
+                double d => d,
+                decimal dc => (double)dc,
+                _ => 0
+            };
+
+            if (val > 0) return new SolidColorBrush(Color.FromRgb(34, 139, 34));
+            if (val < 0) return new SolidColorBrush(Color.FromRgb(220, 20, 60));
+            return new SolidColorBrush(Color.FromRgb(128, 128, 128)); 
         }
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            => throw new NotImplementedException();
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
     }
 }
