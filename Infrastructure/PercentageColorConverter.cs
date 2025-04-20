0f501a131178220e7fb44d157287b08d6dbb5ea5
Lolluckt <egorbl2004@gmail.com>
2025-04-20 15:28:25 +0300

    Improved logic of PercentageColorConverter

--- Infrastructure/PercentageColorConverter.cs
+++ Infrastructure/PercentageColorConverter.cs
@@ -9,18 +9,46 @@ namespace CryptoTrackerApp.Infrastructure
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            double val = value switch
+            if (value is decimal decimalPercent)
             {
-                double d => d,
-                decimal dc => (double)dc,
-                _ => 0
-            };
+                double percent = (double)decimalPercent;
+                if (parameter is string param && param == "Background")
+                {
+                    return percent > 0
+                        ? new SolidColorBrush(Color.FromRgb(33, 192, 95))
+                        : percent < 0
+                            ? new SolidColorBrush(Color.FromRgb(246, 70, 93))
+                            : new SolidColorBrush(Color.FromRgb(154, 161, 176));
+                }
+                return percent > 0
+                    ? new SolidColorBrush(Color.FromRgb(33, 192, 95))
+                    : percent < 0
+                        ? new SolidColorBrush(Color.FromRgb(246, 70, 93))
+                        : new SolidColorBrush(Color.FromRgb(154, 161, 176));
+            }
+            else if (value is double doublePercent)
+            {
+                if (parameter is string param && param == "Background")
+                {
+                    return doublePercent > 0
+                        ? new SolidColorBrush(Color.FromRgb(33, 192, 95))
+                        : doublePercent < 0
+                            ? new SolidColorBrush(Color.FromRgb(246, 70, 93))
+                            : new SolidColorBrush(Color.FromRgb(154, 161, 176));
+                }
 
-            if (val > 0) return new SolidColorBrush(Color.FromRgb(34, 139, 34));
-            if (val < 0) return new SolidColorBrush(Color.FromRgb(220, 20, 60));
-            return new SolidColorBrush(Color.FromRgb(128, 128, 128)); 
+                return doublePercent > 0
+                    ? new SolidColorBrush(Color.FromRgb(33, 192, 95))
+                    : doublePercent < 0
+                        ? new SolidColorBrush(Color.FromRgb(246, 70, 93)) 
+                        : new SolidColorBrush(Color.FromRgb(154, 161, 176));
+            }
+
+            return new SolidColorBrush(Colors.Gray);
         }
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
             throw new NotImplementedException();
+        }
     }
 }
