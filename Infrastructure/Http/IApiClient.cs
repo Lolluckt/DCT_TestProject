6a9c075d9a214a83c82b606df4d8c6880c91a211
Lolluckt <egorbl2004@gmail.com>
2025-04-20 23:36:05 +0300

    Refactor: apply Strategy pattern to decouple API client from service layer

---
+++ Infrastructure/Http/IApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CryptoTrackerApp.Infrastructure.Http
+{
+    public interface IApiClient
+    {
+        Task<JsonElement> GetAsync(string endpoint, Dictionary<string, string> parameters);
+        Task<string> GetRawAsync(string endpoint, Dictionary<string, string> parameters);
+    }
+}
