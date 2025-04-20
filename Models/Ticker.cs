6f3934b3567f5f0bd75a7a24b93e1877e52b29b2
Lolluckt <egorbl2004@gmail.com>
2025-04-20 15:24:22 +0300

    New ticker model

---
+++ Models/Ticker.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoTrackerApp.Models
+{
+    public class Ticker
+    {
+        public string MarketName { get; set; }
+        public string Base { get; set; }
+        public string Target { get; set; }
+        public decimal LastPriceUsd { get; set; }
+        public string TradeUrl { get; set; }
+    }
+}
