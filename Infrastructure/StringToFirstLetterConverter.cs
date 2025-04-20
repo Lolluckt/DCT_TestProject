5819129023fc9997832ff1a30693faed8ef950b9
Lolluckt <egorbl2004@gmail.com>
2025-04-20 15:29:26 +0300

    New StringToFirstLetterConverter

---
+++ Infrastructure/StringToFirstLetterConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CryptoTrackerApp.Infrastructure
+{
+    public class StringToFirstLetterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string text && !string.IsNullOrEmpty(text))
+            {
+                var parts = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length >= 2)
+                {
+                    return $"{parts[0][0]}{parts[1][0]}".ToUpper();
+                }
+                else
+                {
+                    return text.Substring(0, Math.Min(text.Length, 1)).ToUpper();
+                }
+            }
+            return "?";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
