50cad63080e4492f8bdabe57bc9004f79d3e4b5a
Lolluckt <egorbl2004@gmail.com>
2025-04-21 20:58:36 +0300

    New Page with converter

---
+++ Views/ConverterView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using CryptoTrackerApp.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptoTrackerApp.Views
+{
+    public partial class ConverterView : Page
+    {
+        public ConverterView()
+        {
+            InitializeComponent();
+            DataContext = App.ServiceProvider.GetService<ConverterViewModel>();
+        }
+    }
+}
